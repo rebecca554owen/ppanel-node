2c096652f63bd0eecf4a59d1e435ba15be6b6fb7
wyx2685 <wyx2685@gmail.com>
2024-12-17 20:56:14 +0900

    init manage script ppnode

--- Scripts/ppnode.sh
+++ Scripts/ppnode.sh
@@ -5,10 +5,8 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 
-cur_dir=$(pwd)
-
 # check root
-[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
+[[ $EUID -ne 0 ]] && echo -e "${red}错误: ${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
 # check os
 if [[ -f /etc/redhat-release ]]; then
@@ -34,26 +32,6 @@ else
     echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
 fi
 
-arch=$(uname -m)
-
-if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
-    arch="64"
-elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
-    arch="arm64-v8a"
-elif [[ $arch == "s390x" ]]; then
-    arch="s390x"
-else
-    arch="64"
-    echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
-fi
-
-echo "架构: ${arch}"
-
-if [ "$(getconf WORD_BIT)" != '32' ] && [ "$(getconf LONG_BIT)" != '64' ] ; then
-    echo "本软件不支持 32 位系统(x86)，请使用 64 位系统(x86_64)，如果检测有误，请联系作者"
-    exit 2
-fi
-
 # os version
 if [[ -f /etc/os-release ]]; then
     os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
@@ -67,7 +45,7 @@ if [[ x"${release}" == x"centos" ]]; then
         echo -e "${red}请使用 CentOS 7 或更高版本的系统！${plain}\n" && exit 1
     fi
     if [[ ${os_version} -eq 7 ]]; then
-        echo -e "${red}注意： CentOS 7 无法使用hysteria2协议！${plain}\n"
+        echo -e "${red}注意： CentOS 7 无法使用hysteria1/2协议！${plain}\n"
     fi
 elif [[ x"${release}" == x"ubuntu" ]]; then
     if [[ ${os_version} -lt 16 ]]; then
@@ -79,23 +57,221 @@ elif [[ x"${release}" == x"debian" ]]; then
     fi
 fi
 
-install_base() {
-    if [[ x"${release}" == x"centos" ]]; then
-        yum install epel-release -y
-        yum install wget curl unzip tar crontabs socat -y
-        yum install ca-certificates wget -y
-        update-ca-trust force-enable
-    elif [[ x"${release}" == x"debian" ]]; then
-        apt-get update -y
-        apt-get install wget curl unzip tar cron socat ca-certificates -y
-        update-ca-certificates
-    elif [[ x"${release}" == x"ubuntu" ]]; then
-        apt-get update -y
-        apt-get install wget curl unzip tar cron socat ca-certificates -y
-        update-ca-certificates
-    elif [[ x"${release}" == x"arch" ]]; then
-        pacman -Sy
-        pacman -S --noconfirm --needed wget curl unzip tar cron socat ca-certificates
+# 检查系统是否有 IPv6 地址
+check_ipv6_support() {
+    if ip -6 addr | grep -q "inet6"; then
+        echo "1"  # 支持 IPv6
+    else
+        echo "0"  # 不支持 IPv6
+    fi
+}
+
+confirm() {
+    if [[ $# > 1 ]]; then
+        echo && read -rp "$1 [默认$2]: " temp
+        if [[ x"${temp}" == x"" ]]; then
+            temp=$2
+        fi
+    else
+        read -rp "$1 [y/n]: " temp
+    fi
+    if [[ x"${temp}" == x"y" || x"${temp}" == x"Y" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+confirm_restart() {
+    confirm "是否重启PPanel-node" "y"
+    if [[ $? == 0 ]]; then
+        restart
+    else
+        show_menu
+    fi
+}
+
+before_show_menu() {
+    echo && echo -n -e "${yellow}按回车返回主菜单: ${plain}" && read temp
+    show_menu
+}
+
+install() {
+    bash <(curl -Ls https://raw.githubusercontent.com/wyx2685/ppanel-node/master/Scripts/install.sh)
+    if [[ $? == 0 ]]; then
+        if [[ $# == 0 ]]; then
+            start
+        else
+            start 0
+        fi
+    fi
+}
+
+update() {
+    if [[ $# == 0 ]]; then
+        echo && echo -n -e "输入指定版本(默认最新版): " && read version
+    else
+        version=$2
+    fi
+    bash <(curl -Ls https://raw.githubusercontent.com/wyx2685/ppanel-node/master/Scripts/install.sh) $version
+    if [[ $? == 0 ]]; then
+        echo -e "${green}更新完成，已自动重启 PPanel-node，请使用 ppnode log 查看运行日志${plain}"
+        exit
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+config() {
+    echo "PPanel-node在修改配置后会自动尝试重启"
+    vi /etc/PPanel-node/config.json
+    sleep 2
+    restart
+    check_status
+    case $? in
+        0)
+            echo -e "PPanel-node状态: ${green}已运行${plain}"
+            ;;
+        1)
+            echo -e "检测到您未启动PPanel-node或自动重启失败，是否查看日志？[Y/n]" && echo
+            read -e -rp "(默认: y):" yn
+            [[ -z ${yn} ]] && yn="y"
+            if [[ ${yn} == [Yy] ]]; then
+               show_log
+            fi
+            ;;
+        2)
+            echo -e "PPanel-node状态: ${red}未安装${plain}"
+    esac
+}
+
+uninstall() {
+    confirm "确定要卸载 PPanel-node 吗?" "n"
+    if [[ $? != 0 ]]; then
+        if [[ $# == 0 ]]; then
+            show_menu
+        fi
+        return 0
+    fi
+    systemctl stop PPanel-node
+    systemctl disable PPanel-node
+    rm /etc/systemd/system/PPanel-node.service -f
+    systemctl daemon-reload
+    systemctl reset-failed
+    rm /etc/PPanel-node/ -rf
+    rm /usr/local/PPanel-node/ -rf
+
+    echo ""
+    echo -e "卸载成功，如果你想删除此脚本，则退出脚本后运行 ${green}rm /usr/bin/ppnode -f${plain} 进行删除"
+    echo ""
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+start() {
+    check_status
+    if [[ $? == 0 ]]; then
+        echo ""
+        echo -e "${green}PPanel-node已运行，无需再次启动，如需重启请选择重启${plain}"
+    else
+        systemctl start PPanel-node
+        sleep 2
+        check_status
+        if [[ $? == 0 ]]; then
+            echo -e "${green}PPanel-node 启动成功，请使用 ppnode log 查看运行日志${plain}"
+        else
+            echo -e "${red}PPanel-node可能启动失败，请稍后使用 ppnode log 查看日志信息${plain}"
+        fi
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+stop() {
+    systemctl stop PPanel-node
+    sleep 2
+    check_status
+    if [[ $? == 1 ]]; then
+        echo -e "${green}PPanel-node 停止成功${plain}"
+    else
+        echo -e "${red}PPanel-node停止失败，可能是因为停止时间超过了两秒，请稍后查看日志信息${plain}"
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+restart() {
+    systemctl restart PPanel-node
+    sleep 2
+    check_status
+    if [[ $? == 0 ]]; then
+        echo -e "${green}PPanel-node 重启成功，请使用 ppnode log 查看运行日志${plain}"
+    else
+        echo -e "${red}PPanel-node可能启动失败，请稍后使用 ppnode log 查看日志信息${plain}"
+    fi
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+status() {
+    systemctl status PPanel-node --no-pager -l
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+enable() {
+    systemctl enable PPanel-node
+    if [[ $? == 0 ]]; then
+        echo -e "${green}PPanel-node 设置开机自启成功${plain}"
+    else
+        echo -e "${red}PPanel-node 设置开机自启失败${plain}"
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+disable() {
+    systemctl disable PPanel-node
+    if [[ $? == 0 ]]; then
+        echo -e "${green}PPanel-node 取消开机自启成功${plain}"
+    else
+        echo -e "${red}PPanel-node 取消开机自启失败${plain}"
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+show_log() {
+    journalctl -u PPanel-node.service -e --no-pager -f
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+
+update_shell() {
+    wget -O /usr/bin/ppnode -N --no-check-certificate https://raw.githubusercontent.com/wyx2685/ppanel-node/master/Scripts/ppnode.sh
+    if [[ $? != 0 ]]; then
+        echo ""
+        echo -e "${red}下载脚本失败，请检查本机能否连接 Github${plain}"
+        before_show_menu
+    else
+        chmod +x /usr/bin/ppnode
+        echo -e "${green}升级脚本成功，请重新运行脚本${plain}" && exit 0
     fi
 }
 
@@ -112,12 +288,83 @@ check_status() {
     fi
 }
 
-# 检查系统是否有 IPv6 地址
-check_ipv6_support() {
-    if ip -6 addr | grep -q "inet6"; then
-        echo "1"  # 支持 IPv6
+check_enabled() {
+    temp=$(systemctl is-enabled PPanel-node)
+    if [[ x"${temp}" == x"enabled" ]]; then
+        return 0
     else
-        echo "0"  # 不支持 IPv6
+        return 1;
+    fi
+}
+
+check_uninstall() {
+    check_status
+    if [[ $? != 2 ]]; then
+        echo ""
+        echo -e "${red}PPanel-node已安装，请不要重复安装${plain}"
+        if [[ $# == 0 ]]; then
+            before_show_menu
+        fi
+        return 1
+    else
+        return 0
+    fi
+}
+
+check_install() {
+    check_status
+    if [[ $? == 2 ]]; then
+        echo ""
+        echo -e "${red}请先安装PPanel-node${plain}"
+        if [[ $# == 0 ]]; then
+            before_show_menu
+        fi
+        return 1
+    else
+        return 0
+    fi
+}
+
+show_status() {
+    check_status
+    case $? in
+        0)
+            echo -e "PPanel-node状态: ${green}已运行${plain}"
+            show_enable_status
+            ;;
+        1)
+            echo -e "PPanel-node状态: ${yellow}未运行${plain}"
+            show_enable_status
+            ;;
+        2)
+            echo -e "PPanel-node状态: ${red}未安装${plain}"
+    esac
+}
+
+show_enable_status() {
+    check_enabled
+    if [[ $? == 0 ]]; then
+        echo -e "是否开机自启: ${green}是${plain}"
+    else
+        echo -e "是否开机自启: ${red}否${plain}"
+    fi
+}
+
+generate_x25519_key() {
+    echo -n "正在生成 x25519 密钥："
+    /usr/local/PPanel-node/ppnode x25519
+    echo ""
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+show_PPanel-node_version() {
+    echo -n "PPanel-node 版本："
+    /usr/local/PPanel-node/ppnode version
+    echo ""
+    if [[ $# == 0 ]]; then
+        before_show_menu
     fi
 }
 
@@ -473,88 +720,118 @@ EOF
     systemctl restart PPanel-node.service
 }
 
-install_PPanel-node() {
-    if [[ -e /usr/local/PPanel-node/ ]]; then
-        rm -rf /usr/local/PPanel-node/
-    fi
-
-    mkdir /usr/local/PPanel-node/ -p
-    cd /usr/local/PPanel-node/
-
-    if  [ $# == 0 ] ;then
-        last_version=$(curl -Ls "https://api.github.com/repos/wyx2685/PPanel-node/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-        if [[ ! -n "$last_version" ]]; then
-            echo -e "${red}检测 PPanel-node 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 PPanel-node 版本安装${plain}"
-            exit 1
-        fi
-        echo -e "检测到 PPanel-node 最新版本：${last_version}，开始安装"
-        wget -q -N --no-check-certificate -O /usr/local/PPanel-node/PPanel-node-linux.zip https://github.com/wyx2685/PPanel-node/releases/download/${last_version}/PPanel-node-linux-${arch}.zip
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 PPanel-node 失败，请确保你的服务器能够下载 Github 的文件${plain}"
-            exit 1
-        fi
-    else
-        last_version=$1
-        url="https://github.com/wyx2685/PPanel-node/releases/download/${last_version}/PPanel-node-linux-${arch}.zip"
-        echo -e "开始安装 PPanel-node $1"
-        wget -q -N --no-check-certificate -O /usr/local/PPanel-node/PPanel-node-linux.zip ${url}
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 PPanel-node $1 失败，请确保此版本存在${plain}"
-            exit 1
-        fi
-    fi
-
-    unzip PPanel-node-linux.zip
-    rm PPanel-node-linux.zip -f
-    chmod +x ppnode
-    mkdir /etc/PPanel-node/ -p
-    rm /etc/systemd/system/PPanel-node.service -f
-    file="https://raw.githubusercontent.com/wyx2685/ppanel-node/master/Scripts/PPanel-node.service"
-    wget -q -N --no-check-certificate -O /etc/systemd/system/PPanel-node.service ${file}
-    systemctl daemon-reload
-    systemctl stop PPanel-node
-    systemctl enable PPanel-node
-    echo -e "${green}PPanel-node ${last_version}${plain} 安装完成，已设置开机自启"
-    cp geoip.dat /etc/PPanel-node/
-    cp geosite.dat /etc/PPanel-node/
-
-    if [[ ! -f /etc/PPanel-node/config.json ]]; then
-        cp config.json /etc/PPanel-node/
-        echo -e ""
-        echo -e "全新安装，请先参看文档，配置必要的内容"
-        first_install=true
-    else
-        systemctl start PPanel-node
-        sleep 2
-        check_status
-        echo -e ""
-        if [[ $? == 0 ]]; then
-            echo -e "${green}PPanel-node 重启成功${plain}"
-        else
-            echo -e "${red}PPanel-node 启动失败"
-        fi
-        first_install=false
-    fi
-
-    curl -o /usr/bin/ppnode -Ls https://raw.githubusercontent.com/wyx2685/ppanel-node/master/Scripts/ppnode.sh
-    chmod +x /usr/bin/ppnode
-    if [ ! -L /usr/bin/PPnode ]; then
-        ln -s /usr/bin/PPnode /usr/bin/ppnode
-        chmod +x /usr/bin/PPnode
-    fi
-
-    cd $cur_dir
-    rm -f install.sh
-
-    # 首次安装询问是否生成配置文件
-    if [[ $first_install == true ]]; then
-        read -rp "检测到你为第一次安装PPanel-node,是否自动直接生成配置文件？(y/n): " if_generate
-        if [[ $if_generate == [Yy] ]]; then
-            generate_config_file
-        fi
-    fi
+# 放开防火墙端口
+open_ports() {
+    systemctl stop firewalld.service 2>/dev/null
+    systemctl disable firewalld.service 2>/dev/null
+    setenforce 0 2>/dev/null
+    ufw disable 2>/dev/null
+    iptables -P INPUT ACCEPT 2>/dev/null
+    iptables -P FORWARD ACCEPT 2>/dev/null
+    iptables -P OUTPUT ACCEPT 2>/dev/null
+    iptables -t nat -F 2>/dev/null
+    iptables -t mangle -F 2>/dev/null
+    iptables -F 2>/dev/null
+    iptables -X 2>/dev/null
+    netfilter-persistent save 2>/dev/null
+    echo -e "${green}放开防火墙端口成功！${plain}"
 }
 
-echo -e "${green}开始安装${plain}"
-install_base
-install_PPanel-node $1
+show_usage() {
+    echo "PPanel-node 管理脚本使用方法: "
+    echo "------------------------------------------"
+    echo "ppnode              - 显示管理菜单 (功能更多)"
+    echo "ppnode start        - 启动 PPanel-node"
+    echo "ppnode stop         - 停止 PPanel-node"
+    echo "ppnode restart      - 重启 PPanel-node"
+    echo "ppnode status       - 查看 PPanel-node 状态"
+    echo "ppnode enable       - 设置 PPanel-node 开机自启"
+    echo "ppnode disable      - 取消 PPanel-node 开机自启"
+    echo "ppnode log          - 查看 PPanel-node 日志"
+    echo "ppnode x25519       - 生成 x25519 密钥"
+    echo "ppnode generate     - 生成 PPanel-node 配置文件"
+    echo "ppnode update       - 更新 PPanel-node"
+    echo "ppnode update x.x.x - 安装 PPanel-node 指定版本"
+    echo "ppnode install      - 安装 PPanel-node"
+    echo "ppnode uninstall    - 卸载 PPanel-node"
+    echo "ppnode version      - 查看 PPanel-node 版本"
+    echo "------------------------------------------"
+}
+
+show_menu() {
+    echo -e "
+  ${green}PPanel-node 后端管理脚本，${plain}${red}不适用于docker${plain}
+--- https://github.com/wyx2685/PPanel-node ---
+  ${green}0.${plain} 修改配置
+————————————————
+  ${green}1.${plain} 安装 PPanel-node
+  ${green}2.${plain} 更新 PPanel-node
+  ${green}3.${plain} 卸载 PPanel-node
+————————————————
+  ${green}4.${plain} 启动 PPanel-node
+  ${green}5.${plain} 停止 PPanel-node
+  ${green}6.${plain} 重启 PPanel-node
+  ${green}7.${plain} 查看 PPanel-node 状态
+  ${green}8.${plain} 查看 PPanel-node 日志
+————————————————
+  ${green}9.${plain} 设置 PPanel-node 开机自启
+  ${green}10.${plain} 取消 PPanel-node 开机自启
+————————————————
+  ${green}11.${plain} 一键安装 bbr (最新内核)
+  ${green}12.${plain} 查看 PPanel-node 版本
+  ${green}13.${plain} 生成 X25519 密钥
+  ${green}14.${plain} 升级 PPanel-node 维护脚本
+  ${green}15.${plain} 生成 PPanel-node 配置文件
+  ${green}16.${plain} 放行 VPS 的所有网络端口
+  ${green}17.${plain} 退出脚本
+ "
+ #后续更新可加入上方字符串中
+    show_status
+    echo && read -rp "请输入选择 [0-17]: " num
+
+    case "${num}" in
+        0) config ;;
+        1) check_uninstall && install ;;
+        2) check_install && update ;;
+        3) check_install && uninstall ;;
+        4) check_install && start ;;
+        5) check_install && stop ;;
+        6) check_install && restart ;;
+        7) check_install && status ;;
+        8) check_install && show_log ;;
+        9) check_install && enable ;;
+        10) check_install && disable ;;
+        11) install_bbr ;;
+        12) check_install && show_PPanel-node_version ;;
+        13) check_install && generate_x25519_key ;;
+        14) update_shell ;;
+        15) generate_config_file ;;
+        16) open_ports ;;
+        17) exit ;;
+        *) echo -e "${red}请输入正确的数字 [0-16]${plain}" ;;
+    esac
+}
+
+
+if [[ $# > 0 ]]; then
+    case $1 in
+        "start") check_install 0 && start 0 ;;
+        "stop") check_install 0 && stop 0 ;;
+        "restart") check_install 0 && restart 0 ;;
+        "status") check_install 0 && status 0 ;;
+        "enable") check_install 0 && enable 0 ;;
+        "disable") check_install 0 && disable 0 ;;
+        "log") check_install 0 && show_log 0 ;;
+        "update") check_install 0 && update 0 $2 ;;
+        "config") config $* ;;
+        "generate") generate_config_file ;;
+        "install") check_uninstall 0 && install 0 ;;
+        "uninstall") check_install 0 && uninstall 0 ;;
+        "x25519") check_install 0 && generate_x25519_key 0 ;;
+        "version") check_install 0 && show_PPanel-node_version 0 ;;
+        "update_shell") update_shell ;;
+        *) show_usage
+    esac
+else
+    show_menu
+fi
